4cece9c5bebaf422232e3e51dc3dc4f184a29bef
Enzuz-s <mitchellbakx11@gmail.com>
2021-05-12 20:35:14 +0200

    :

--- MadLibs.py
+++ MadLibs.py
@@ -1,14 +1,14 @@
-from random import randint
 import copy
 import time
+from random import randint
 
 story = (
-    "One day my {} friend and I decided to go to the {} game in {}. " +
-    "We really wanted to see the {} play the {}. " +
-    "So, we {} our {} down to the {} and bought some {}s. " +
-    "We got into the game and it was a lot of fun. " +
-    "We ate some {} {} and drank some {} {}. " +
-    "We had a great time! We plan to go again next year!"
+        "One day my {} friend and I decided to go to the {} game in {}. " +
+        "We really wanted to see the {} play the {}. " +
+        "So, we {} our {} down to the {} and bought some {}s. " +
+        "We got into the game and it was a lot of fun. " +
+        "We ate some {} {} and drank some {} {}. " +
+        "We had a great time! We plan to go again next year!"
 )
 
 word_dict = {
@@ -22,7 +22,7 @@ word_dict = {
 
 def get_word(type, local_dict):
     words = local_dict[type]
-    cnt = len(words)-1
+    cnt = len(words) - 1
     index = randint(0, cnt)
     return local_dict[type].pop(index)
 
